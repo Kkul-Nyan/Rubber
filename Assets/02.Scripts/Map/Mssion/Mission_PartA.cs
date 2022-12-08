b415fb1b6bd8ae107ba620852f91cfe022e5129e
YEONJOODA <duswn0018@naver.com>
2022-12-08 17:54:58 +0900

    [Replace]MapMissionScriptA_1.0.1
    스크립트 조금 추가, 모델링 위치 조정 및 기능 추가

--- Assets/02.Scripts/Map/Mssion/Mission_PartA.cs
+++ Assets/02.Scripts/Map/Mssion/Mission_PartA.cs
@@ -8,32 +8,40 @@ public class Mission_PartA : MonoBehaviour
     float timer = 0;
     private void OnCollisionStay(Collision collision)
     {
-        
+
         timer += Time.deltaTime;
         Renderer render;
         render = gameObject.GetComponent<Renderer>();
         bool cleanPlate = false;
+        bool cleanFountain = false;
 
         // 식당 - 설거지
         if (collision.gameObject.tag == "SOAP" && changeColor <= 1 && timer > 2)
         {
             changeColor -= 0.3f;
             Debug.Log("Enter");
-            render.material.color = Color.LerpUnclamped(Color.white, Color.black, changeColor);
+            render.material.color = Color.LerpUnclamped(Color.black, Color.white, changeColor);
             timer = 0;
         }
-        // 분수대 - 닦기
-        else if (collision.gameObject.tag == "DUSTCLOTH" && changeColor <= 1 && timer > 2)
-        {
-            changeColor -= 0.3f;
-            Debug.Log("Enter");
-            render.material.color = Color.LerpUnclamped(Color.white, Color.black, changeColor);
-            timer = 0;
-        }
-        else if(changeColor <= 0)
+        else if (changeColor <= 0)
         {
             cleanPlate = true;
             Debug.Log("cleanPlate = " + cleanPlate);
         }
+
+        // 대공원 - 분수닦기
+        if (collision.gameObject.tag == "FOUNTAIN" && changeColor <= 1 && timer > 2)
+        {
+                changeColor -= 0.3f;
+                Debug.Log("Enter");
+                render.material.color = Color.LerpUnclamped(Color.black, Color.gray, changeColor);
+                timer = 0;
+        }
+        else if (changeColor <= 0)
+        {
+            cleanFountain = true;
+            Debug.Log("cleanFountain = " + cleanFountain);
+        }
+
     }
 }
