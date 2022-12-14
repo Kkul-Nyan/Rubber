b97a48c2484ce3c165a9a2572ac8d01106c5a9f0
kimss018 <106726051+kimss018@users.noreply.github.com>
2022-12-14 11:46:28 +0900

    [Add]MapMinssion_Minifarm
    텃밭 스크립트 작성, 상자 추가, 태그추가

---
+++ Assets/02.Scripts/Map/Mssion/MiniFarm/Vegetable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetable : MonoBehaviour
+{
+    public string vegetableName;
+    public int vegetableID;
+}
